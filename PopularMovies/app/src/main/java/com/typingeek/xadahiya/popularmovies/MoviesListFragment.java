aa6af441d9b193c6121d8020bc33e211b326f45e
xadahiya <xadahiya@gmail.com>
2016-07-15 21:01:56 +0530

    refracted gridAdapter code to gridAdapter.class, will need to implement a custom adapter

--- PopularMovies/app/src/main/java/com/typingeek/xadahiya/popularmovies/MoviesListFragment.java
+++ PopularMovies/app/src/main/java/com/typingeek/xadahiya/popularmovies/MoviesListFragment.java
@@ -31,7 +31,7 @@ public class MoviesListFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.activity_movies_list_fragment, container, false);
         GridView gridview = (GridView) v.findViewById(R.id.movies_gridview);
-        gridview.setAdapter(new ImageAdapter(getActivity().getApplicationContext()));
+        gridview.setAdapter(new GridAdapter(getActivity().getApplicationContext()));
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
@@ -41,58 +41,6 @@ public class MoviesListFragment extends Fragment {
         });
         return v;
 
-    }
-
-    public class ImageAdapter extends BaseAdapter {
-        private Context mContext;
-
-        public ImageAdapter(Context c) {
-            mContext = c;
-        }
-
-        public int getCount() {
-            return mThumbIds.length;
-        }
-
-        public Object getItem(int position) {
-            return null;
-        }
-
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        // create a new ImageView for each item referenced by the Adapter
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            if (convertView == null) {
-                // if it's not recycled, initialize some attributes
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(8, 8, 8, 8);
-            } else {
-                imageView = (ImageView) convertView;
-            }
-
-            imageView.setImageResource(mThumbIds[position]);
-            return imageView;
-        }
-
-        // references to our images
-        private Integer[] mThumbIds = {
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7,
-                R.drawable.sample_0, R.drawable.sample_1,
-                R.drawable.sample_2, R.drawable.sample_3,
-                R.drawable.sample_4, R.drawable.sample_5,
-                R.drawable.sample_6, R.drawable.sample_7
-        };
     }
 
 
